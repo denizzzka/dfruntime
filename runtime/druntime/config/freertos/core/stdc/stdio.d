fbd0f0925e7f61c3dc9a6a23e3da8a7ea9664942
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-11 05:37:57 +0300

    FreeRTOS: stdio.d compilation fix

---
+++ runtime/druntime/config/freertos/core/stdc/stdio.d
@@ -0,0 +1,44 @@
+module external.libc.stdio_;
+
+import core.stdc.config: c_long;
+
+///
+alias fpos_t = c_long;
+
+///
+alias _iobuf = FILE;
+
+enum
+{
+    ///
+    _IOFBF = 0,
+    ///
+    _IOLBF = 1,
+    ///
+    _IONBF = 2,
+}
+
+///
+struct FILE;
+
+__gshared FILE* stdin;
+__gshared FILE* stdout;
+__gshared FILE* stderr;
+
+enum
+{
+    ///
+    BUFSIZ       = 4096,
+    ///
+    EOF          = -1,
+    ///
+    FOPEN_MAX    = 16,
+    ///
+    FILENAME_MAX = 4095,
+    ///
+    TMP_MAX      = 238328,
+    ///
+    L_tmpnam     = 20
+}
+
+extern(C) int snprintf(scope char* s, size_t n, scope const char* format, ...) nothrow @nogc;
