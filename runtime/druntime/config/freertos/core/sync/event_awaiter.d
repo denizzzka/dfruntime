f48ecf09a34b70cc744a8edce8d43f534531eb28
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-11 07:45:57 +0300

    FreeRTOS: module core.sync.event_awaiter implemented

---
+++ runtime/druntime/config/freertos/core/sync/event_awaiter.d
@@ -0,0 +1,123 @@
+module core.sync.event_awaiter;
+
+static import os = freertos_binding;
+import core.time;
+import core.internal.abort : abort;
+
+struct EventAwaiter
+{
+    private os.EventGroupHandle_t group;
+    private os.BaseType_t clearOnExit;
+
+    nothrow @nogc:
+
+    this(bool manualReset, bool initialState) @trusted
+    {
+        import core.internal.abort: abort;
+
+        group = os.xEventGroupCreate();
+
+        if(group is null)
+            abort("xEventGroupCreate failed");
+
+        clearOnExit = manualReset ? os.pdFALSE : os.pdTRUE;
+
+        if(initialState)
+            set();
+    }
+
+    ~this() @trusted
+    in(group)
+    {
+        os.vEventGroupDelete(group);
+        group = null;
+    }
+
+    // copying not allowed, can produce resource leaks
+    @disable this(this);
+    @disable void opAssign(EventAwaiter);
+
+    void set()
+    in(group)
+    {
+        os.xEventGroupSetBits(group, BITS_MASK);
+    }
+
+    void reset()
+    in(group)
+    {
+        os.xEventGroupClearBits(group, BITS_MASK);
+    }
+
+    void wait()
+    in(group)
+    {
+        uint r;
+
+        // xEventGroupWaitBits can return immediately on some cases:
+        // https://www.freertos.org/FreeRTOS_Support_Forum_Archive/February_2018/freertos_xEventGroupWaitBits_unexpected_behavior_cd13225cj.html
+        do
+        {
+            r = os.xEventGroupWaitBits(
+               group,
+               BITS_MASK,
+               clearOnExit,
+               os.pdFALSE, // xWaitForAllBits
+               os.portMAX_DELAY // xTicksToWait
+            );
+        }
+        while(!r);
+
+        assert(r & BITS_MASK);
+    }
+
+    bool wait(Duration tmout)
+    in(!tmout.isNegative)
+    in(group)
+    {
+        import external.core.time: currTicks;
+
+        /*
+        If the task that is waiting for event bits is also being suspended and
+        resumed then you could check the time before calling
+        xEventGroupWaitBits(), and then if the function returns without any bits
+        being set, check the time again to know if the function returned because
+        of a timeout.  If the requested block time has not expired, and no bits
+        are set, then you could assume the function returned because the task
+        got suspended and then resumed again.
+
+        Posted by rtel on February 8, 2018
+        */
+
+        const timeoutTicks = tmout.toTicks();
+        assert(timeoutTicks < uint.max);
+
+        long startTime = currTicks();
+        const endTime = startTime + timeoutTicks;
+
+        do
+        {
+            long dt = endTime - startTime;
+
+            assert(dt > 0);
+
+            auto r = os.xEventGroupWaitBits(
+               group,
+               BITS_MASK,
+               clearOnExit,
+               os.pdFALSE, // xWaitForAllBits
+               cast(uint) dt // xTicksToWait
+            );
+
+            if(r & BITS_MASK)
+                return true;
+
+            startTime = currTicks();
+        }
+        while(startTime < endTime);
+
+        return false;
+    }
+}
+
+//TODO: implement common unittests for all Event implementations
