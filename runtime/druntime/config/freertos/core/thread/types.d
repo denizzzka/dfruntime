95c5967f292f15cbf9ca7b2fbf393b598f69bc89
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-13 16:09:54 +0300

    Freertos: forgot to add core/thread/types.d

---
+++ runtime/druntime/config/freertos/core/thread/types.d
@@ -0,0 +1,53 @@
+module core.thread.types;
+
+import freertos_binding: TaskHandle_t;
+public import core.thread.stack: isStackGrowingDown;
+
+alias ThreadID = TaskHandle_t;
+
+struct ll_ThreadData
+{
+    //~ FIXME:
+    //~ import core.event: Event;
+    import core.atomic;
+
+    ThreadID tid;
+    //~ Event joinEvent;
+    private shared size_t joinEventSubscribersNum;
+
+    void initialize() nothrow @nogc
+    {
+        //~ joinEvent.initialize(true, false);
+    }
+
+    auto getSubscribersNum()
+    {
+        return atomicLoad(joinEventSubscribersNum);
+    }
+
+    void deletionLock() @nogc nothrow
+    {
+        //~ joinEventSubscribersNum.atomicOp!"+="(1);
+    }
+
+    void deletionUnlock() @nogc nothrow
+    {
+        //~ joinEventSubscribersNum.atomicOp!"-="(1);
+    }
+}
+
+unittest
+{
+    ll_ThreadData td;
+    td.initialize();
+
+    assert(td.getSubscribersNum() == 0);
+    td.deletionLock();
+    assert(td.getSubscribersNum() == 1);
+    td.deletionLock();
+    assert(td.getSubscribersNum() == 2);
+    td.deletionUnlock();
+    assert(td.getSubscribersNum() == 1);
+    td.deletionUnlock();
+    assert(td.getSubscribersNum() == 0);
+}
