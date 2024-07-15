9b42f352a450693a971d3cbfba3c3a08475f321e
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-15 16:24:41 +0300

    FreeRTOS: rt.cover_impl stub added

---
+++ runtime/druntime/config/freertos/rt/cover_impl.d
@@ -0,0 +1,33 @@
+module rt.cover_impl;
+
+package:
+
+import core.stdc.stdio;
+
+void setFileLen(T)(ref T flst)
+{
+    assert(false, __FUNCTION__~" not implemented");
+}
+
+const char sep = '/';
+
+string getExt(string name)
+{
+    assert(false, __FUNCTION__~" not implemented");
+}
+
+FILE* openFile(string name)
+{
+    assert(false, __FUNCTION__~" not implemented");
+}
+
+// open/create file for read/write, pointer at beginning
+FILE* openOrCreateFile(string name)
+{
+    assert(false, __FUNCTION__~" not implemented");
+}
+
+void lockFile(FILE* fd)
+{
+    assert(false, __FUNCTION__~" not implemented");
+}
