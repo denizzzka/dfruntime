9b0a1d76297ea8c7022238ebb3f56d2fbbe69558
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-15 17:25:55 +0300

    FreeRTOS: rt.monitor_mutex implemented

---
+++ runtime/druntime/config/freertos/rt/monitor_mutex.d
@@ -0,0 +1,48 @@
+/**
+ * FreeRTOS implementation for object monitors mutexes.
+ *
+ * Copyright: Copyright Denis Feklushkin 2024.
+ * License: Distributed under the
+ *      $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0).
+ *    (See accompanying file LICENSE)
+ * Authors:   Denis Feklushkin
+ * Source: $(DRUNTIMESRC config/freertos/rt/monitor_mutex.d)
+ */
+module rt.monitor_mutex;
+
+package:
+nothrow:
+@nogc:
+
+import internal.binding;
+
+private alias MonitorMutex = SemaphoreHandle_t;
+alias Mutex = MonitorMutex;
+
+void initMutex(MonitorMutex* mtx)
+{
+    *mtx = _xSemaphoreCreateMutex();
+
+    if(*mtx is null)
+        onOutOfMemoryError();
+}
+
+void destroyMutex(MonitorMutex* mtx)
+{
+    _vSemaphoreDelete(*mtx);
+}
+
+void lockMutex(MonitorMutex* mtx)
+{
+    if(xSemaphoreTake(*mtx, portMAX_DELAY) != pdTRUE)
+        onInvalidMemoryOperationError();
+}
+
+void unlockMutex(MonitorMutex* mtx)
+{
+    if(_xSemaphoreGive(*mtx) != pdTRUE)
+        onInvalidMemoryOperationError();
+}
+
+void initMutexesFacility() {}
+void destroyMutexesFacility() {}
