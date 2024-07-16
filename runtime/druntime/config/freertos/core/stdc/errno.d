e76c34e5cce199cd2e060e7c7e60fb1a7326b5d6
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-16 08:15:23 +0300

    Freertos: core.stdc.errno compilation fix

--- runtime/druntime/config/freertos/core/stdc/errno.d
+++ runtime/druntime/config/freertos/core/stdc/errno.d
@@ -5,12 +5,13 @@ nothrow:
 
 extern(C) extern __gshared int errno;
 
+//TODO: remove?
 extern (C) ref int __error() @system
 {
     return errno;
 }
 
-ref int fakePureErrno() @nogc nothrow pure @system
+ref int fakePureErrno() @nogc nothrow @system
 {
     return __error();
 }
