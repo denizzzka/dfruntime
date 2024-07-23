c05c78da0a42239d2521740a3197179452313e56
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-23 15:02:46 +0300

    fenv: FE_* values for ARM32 added

--- runtime/druntime/config/freertos/core/stdc/fenv.d
+++ runtime/druntime/config/freertos/core/stdc/fenv.d
@@ -12,3 +12,18 @@ import core.stdc.config: c_ulong;
 alias fexcept_t = c_ulong;
 
 enum FE_DFL_ENV = cast(fenv_t*)(-1);
+
+version (ARM)
+{
+    enum FE_INEXACT =   0x0010;
+    enum FE_UNDERFLOW = 0x0008;
+    enum FE_OVERFLOW =  0x0004;
+    enum FE_DIVBYZERO = 0x0002;
+    enum FE_INVALID =   0x0001;
+
+    // if VFP is supported:
+    enum FE_TONEAREST =     0x00000000;
+    enum FE_DOWNWARD =      0x00800000;
+    enum FE_UPWARD =        0x00400000;
+    enum FE_TOWARDZERO =    0x00c00000;
+}
