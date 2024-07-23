6b26865a7ffd953d840f49505f9c3da7187f1c15
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-23 13:55:34 +0300

    extern fputwc and fgetwc added

--- runtime/druntime/config/freertos/core/stdc/wchar_.d
+++ runtime/druntime/config/freertos/core/stdc/wchar_.d
@@ -1,6 +1,14 @@
 ///
 module core.stdc.wchar_;
 
+nothrow:
+@nogc:
+
 public import core.stdc.stddef: wchar_t;
 
 alias wint_t = wchar_t;
+
+import core.stdc.stdio: _iobuf;
+
+extern(C) int fputwc(wchar_t c, _iobuf* fp);
+extern(C) int fgetwc(_iobuf* fp);
