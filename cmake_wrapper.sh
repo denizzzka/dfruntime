1cda1a561f75aab21c31677aa06a3a401cb0a113
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-07-27 21:46:49 +0300

    Meson builds Phobos

--- cmake_wrapper.sh
+++ cmake_wrapper.sh
@@ -4,10 +4,13 @@ set -euox pipefail
 
 export CMAKE_INSTALL_MODE=SYMLINK_OR_COPY
 
+# ninja target
+TGT=$1
+
 # -B value place according to meson.build
-BUILD_DIR=$4
+BUILD_DIR=$5
 
-cmake "$@"
+cmake "${@:1}"
 
-ninja -C "$BUILD_DIR" -j8 druntime-ldc
+ninja -C "$BUILD_DIR" -j8 "$TGT"
 cp "$BUILD_DIR/lib/"* "$BUILD_DIR/"
